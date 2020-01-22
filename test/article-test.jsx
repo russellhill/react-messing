a80e0013798c7ae98ce14cdc1352e1a064ad4eaa
Russell Hill <russell.hill@gmail.com>
2020-01-22 07:24:19 +0000

    add testing

--- test/article-test.jsx
+++ test/article-test.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
-import { configure, shallow } from 'enzyme';
+import axe from 'axe-core';
+import { configure, shallow, mount } from 'enzyme';
 import { expect } from 'chai';
 import Adapter from 'enzyme-adapter-react-16';
 import Article from '../src/client/components/Article.jsx';
+import { mountToDoc } from './test-helpers';
 
 configure({ adapter: new Adapter() });
 
 describe('Article component testing', () => {
-    it('renders article data correctly', () => {
-        const dummyArticle = {
-            Title: 'This is a test title',
-            SnippetText: 'Some snippet text',
-            Copy: 'Some copy text',
-            Image: {
-                'small-phone': 'small-phone',
-                'medium-phone': 'medium-phone',
-                tablet: 'tablet',
-                desktop: 'desktop',
-            },
-            Author: {
-                Name: 'Test Author',
-                JobTitle: 'Job Title',
-            },
-        };
+    const dummyArticle = {
+        Title: 'This is a test title',
+        SnippetText: 'Some snippet text',
+        Copy: 'Some copy text',
+        Image: {
+            'small-phone': 'small-phone',
+            'medium-phone': 'medium-phone',
+            tablet: 'tablet',
+            desktop: 'desktop',
+        },
+        Author: {
+            Name: 'Test Author',
+            JobTitle: 'Job Title',
+        },
+    };
 
+    it('renders article data correctly', (done) => {
         const wrapper = shallow(<Article article={dummyArticle} />);
 
         expect(wrapper.find('div .card__title').html()).to.contain(dummyArticle.Title);
@@ -35,5 +37,18 @@ describe('Article component testing', () => {
         expect(wrapper.find('p .card__title--author').html()).to.contain(dummyArticle.Author.JobTitle);
         expect(wrapper.find('span .card__text--snippet').html()).to.contain(dummyArticle.SnippetText);
         expect(wrapper.find('p .card__text').html()).to.contain(dummyArticle.Copy);
+        done();
+    });
+
+    it('accessibility', (done) => {
+        const wrapper = mountToDoc(React.createElement(Article, { article: dummyArticle }));
+        const nodeArticle = wrapper.getDOMNode();
+
+        axe.run(nodeArticle, {}, (err, { violations }) => {
+            console.log('err, violations', err, violations);
+            expect(err).to.equal(null);
+            expect(violations.length).to.equal(0);
+            done();
+        });
     });
 });
